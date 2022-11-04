c56047dc2c7dd6f79537b0896b1e22282864811f
alluardj <alluardj@csvdc.qc.ca>
2022-11-04 14:19:07 -0400

    feat: add a fact table that tracks the number of part-time employees in the last 10 years

---
+++ models/features/fact_tps_partial.sql
@@ -0,0 +1,9 @@
+SELECT
+    YEAR(date_entr) AS 'annee'
+    , lieu_trav AS 'ecole'
+    , COUNT(DISTINCT matr) AS 'total_employes'
+FROM {{ adapt('interfaces', 'i_pai_dos') }}
+WHERE
+    (stat_eng = 'E3')                             -- Statut d'emploi | E3 => Temps partiel
+    and (YEAR(GETDATE()) - YEAR(date_entr) <= 10) -- 10 dernières années    
+GROUP BY date_entr, lieu_trav
