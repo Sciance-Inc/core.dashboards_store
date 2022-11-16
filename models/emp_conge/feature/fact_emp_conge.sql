bd32a521b0fca6ecd368b71a7e07f0eeecb96341
sadqim <sadqim@csvdc.qc.ca>
2022-11-16 08:38:45 -0500

    Chore: move_filter_from_interface_table_to_fact_table

--- models/emp_conge/feature/fact_emp_conge.sql
+++ models/emp_conge/feature/fact_emp_conge.sql
@@ -1,21 +1,43 @@
-with employes as (
-    select DISTINCT
+WITH employes AS (
+    SELECT DISTINCT
         CASE 
             WHEN MONTH(emp.date_eff) < 8 THEN YEAR(emp.date_eff) - 1 
             ELSE YEAR(emp.date_eff)
         END AS 'annee'
         , emp.matr
-        , emp.lieu_trav as 'ecole_code'
-        , lieu.descr as 'ecole_description'
-        , emp.corp_empl as 'corps_emploi'
-        , corpe.descr as 'corps_demploi_description'
+        , emp.lieu_trav AS 'ecole_code'
+        , lieu.descr AS 'ecole_description'
+        , emp.corp_empl AS 'corps_emploi'
+        , corpe.descr AS 'corps_demploi_description'
         , emp.etat
-        , etat.descr as 'etat_description'
-    from {{ ref('stg_paie_hemp') }} as emp
+        , etat.descr AS 'etat_description'
+    FROM {{ ref('stg_paie_hemp') }} AS emp
 
-    inner join {{ ref('stg_pai_tab_etat_empl_conge') }} as etat on emp.etat = etat.etat_empl
-    inner join {{ ref('stg_pai_tab_corp_empl') }} as corpe on emp.corp_empl = corpe.corp_empl
-    inner join {{ ref('stg_pai_tab_lieu_trav') }}  as lieu on emp.lieu_trav = lieu.lieu_trav    
+    INNER JOIN {{ ref('stg_pai_tab_etat_empl_conge') }} AS etat 
+        ON emp.etat = etat.etat_empl
+    INNER JOIN {{ ref('stg_pai_tab_corp_empl') }} AS corpe 
+        ON emp.corp_empl = corpe.corp_empl
+    INNER JOIN {{ ref('stg_pai_tab_lieu_trav') }}  AS lieu 
+        ON emp.lieu_trav = lieu.lieu_trav    
+    WHERE
+        (
+            etat.etat_empl = 'A02' OR
+            etat.etat_empl = 'A07' OR
+            etat.etat_empl = 'A08' OR 
+            etat.etat_empl = 'A11' OR 
+            etat.etat_empl = 'A13' OR 
+            etat.etat_empl = 'A20' OR 
+            etat.etat_empl = 'A21' OR 
+            etat.etat_empl = 'P01' OR 
+            etat.etat_empl = 'P03' OR 
+            etat.etat_empl = 'P04' OR 
+            etat.etat_empl = 'P06' OR 
+            etat.etat_empl = 'S01' OR 
+            etat.etat_empl = 'S02' OR 
+            etat.etat_empl = 'S09' OR 
+            etat.etat_empl = 'S16' OR 
+            etat.etat_empl = 'S20'
+            )       
 )
 
 select * from employes
