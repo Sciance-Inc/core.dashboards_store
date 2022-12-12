a7eea476ea403228bdb3fba25b9fa884f6c8874f
sadqim <sadqim@csvdc.qc.ca>
2022-12-12 14:47:03 -0500

    feat: add_id_friendly_name

---
+++ models/res_etapes/dimensions/rstps_dim_unique_subj_evaluation.sql
@@ -0,0 +1,8 @@
+
+
+
+select distinct 
+    {{ dbt_utils.surrogate_key(['annee', 'friendly_name']) }} as id_friendly_name
+    ,annee
+    ,friendly_name
+FROM {{ ref('rstps_fact_evaluations_grades_from_dim') }}
