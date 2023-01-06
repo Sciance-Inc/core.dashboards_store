645563a5639eadd43b6c92f2a8db7574580696d3
alluardj <alluardj@csvdc.qc.ca>
2023-01-06 10:05:46 -0500

    chore: modification of the i_geobus_parc script so that it respects the conventions of the project (part2).

--- models/shared/interfaces/geobus/i_geobus_parc.sql
+++ models/shared/interfaces/geobus/i_geobus_parc.sql
@@ -10,11 +10,11 @@ SELECT
   , Duree AS duree                                  -- financ
   , NbPlace AS nb_place                             -- circuits rapp
   , IndAvisTransp AS ind_avis_transp
-  , NoCirc AS no_circ                               -- parcour rapport
-  , NoSeqTrt AS no_seq_trt                          -- parcour rapport
+  , NoCirc AS no_circ                               -- parcours rapport
+  , NoSeqTrt AS no_seq_trt                          -- parcours rapport
   , HreDeb AS hre_deb                               -- financ rapport
   , DistImpro AS dist_impro
-  , DureeImpro AS duree_impro                       -- duration sec. for finansial calculation
-  , DateModifParAttrib AS date_modif_par_attrib     -- this date include in the parcoure rapport in a geobus app
+  , DureeImpro AS duree_impro                       -- duration sec. for financial calculation
+  , DateModifParAttrib AS date_modif_par_attrib     -- this date include in the parcours rapport in a geobus app
   FROM 
-    {{ var("database_geobus") }}.dbo.GEO_P_PARC
+    {{ var("database_geobus") }}.dbo.geo_p_parc
