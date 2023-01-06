97b0fd334a3d567a558754952ff1e71e1f7d2ce2
alluardj <alluardj@csvdc.qc.ca>
2023-01-06 10:11:29 -0500

    chore: modif of the stg_parc_activ_10y script so that it respects the conventions of the project

--- models/transport/pbi_tables/stg_parc_activ_10y.sql
+++ models/transport/pbi_tables/stg_parc_activ_10y.sql
@@ -1,18 +1,29 @@
-SELECT distinct p.Annee
-      ,CASE WHEN P.Per = 1 THEN 'AM' WHEN P.Per = 8 THEN 'PM' END AS Per 
-	,P.Per AS PerNum
-      ,LEFT(REPLACE(STR(p.NoParc, 5), ' ', '0'), 5) AS 'Parcours'
-	,p.No_Parc 
-      ,p.Nom
-	,CASE WHEN p.Ind_Active = 1 THEN 'Oui' WHEN p.IndActive = 0 THEN 'No' END AS Actif
-	,LEFT(REPLACE(STR(P.HreDeb, 4), ' ', '0'), 2) + ':' + RIGHT(REPLACE(STR(P.HreDeb, 4), ' ', '0'), 2) as 'Hre_debut'
-      ,p.No_Circ as Circuit
-      ,p.Nb_Place as Capacite
-	,p.Nb_Rang
-	,p.Duree as "Duree_sec"
-      ,p.Dist_Tot as "Dist_m"
-	,p.Dist_Impro
-	,p.Date_Modif_Par_Attrib  as "Maj_attribution"
-  FROM {{ ref('i_geobus_parc') }} as p
-  where p.Ind_Active = 1  and p.Dist_Tot > 0 and p.Duree > 0 and p.simul = 0
-  and p.Annee >=  YEAR(GETDATE())-10
+SELECT DISTINCT 
+      annee
+      , CASE 
+            WHEN per = 1 THEN 'AM' 
+            WHEN per = 8 THEN 'PM' 
+      END AS per 
+	, per AS per_num
+      , LEFT(REPLACE(STR(no_parc, 5), ' ', '0'), 5) AS parcours
+	, no_parc 
+      , nom
+	, CASE 
+            WHEN ind_active = 1 THEN 'Oui' 
+            WHEN ind_active = 0 THEN 'Non' 
+      END AS actif
+	, LEFT(REPLACE(STR(hre_deb, 4), ' ', '0'), 2) + ':' + RIGHT(REPLACE(STR(hre_deb, 4), ' ', '0'), 2) AS hre_debut
+      , no_circ AS circuit
+      , nb_place AS capacite
+	, nb_rang
+	, duree AS duree_sec
+      , dist_tot AS dist_m
+	, dist_impro
+	, date_modif_par_attrib AS maj_attribution
+  FROM {{ ref('i_geobus_parc') }}
+  WHERE 
+      ind_active = 1  
+      AND dist_tot > 0 
+      AND duree > 0 
+      AND simul = 0
+      AND annee >=  YEAR(GETDATE())-10
