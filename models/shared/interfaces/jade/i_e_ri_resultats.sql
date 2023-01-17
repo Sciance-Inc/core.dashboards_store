b893ce435450cbc8ce509f8f632fb97cadd555b8
sadqim <sadqim@csvdc.qc.ca>
2023-01-17 09:22:35 -0500

    Commit a3cb1dff: feat : add_gouv_evaluation_result_to_interface_table

---
+++ models/shared/interfaces/jade/i_e_ri_resultats.sql
@@ -0,0 +1,19 @@
+SELECT 
+    CodePerm
+	, Fiche AS fiche
+    , NomAriane AS nom
+    , PnomLegal AS prenom
+    , EcoCenOff AS ecole
+    , MatCharl AS matiere 
+    , AnneeSanct AS annee_resultat
+    , MoisSanct AS mois_resultat
+    , DateObtensionRes AS date_resultat
+    , GrpCharl AS groupe
+    , ResEcoBrute AS resecobrute
+    , ResEcoMod AS resecomod
+    , ResOffBrute AS resoffbrute
+    , ResOffConv AS resoffconv
+    , ResOffFinal AS resofffinal
+    , Annee AS annee
+FROM 
+    {{ var("database_jade") }}.dbo.E_RI_Resultats
