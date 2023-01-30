aad680e32ce55ac1f3670642c2b2655c4cd8faa6
Jérémie Alluard <alluardj@csvdc.qc.ca>
2023-01-30 14:33:19 +0000

    Merged PR 272: Correctifs afin d'harmoniser le process
    Ci-dessous un récapitulatif des modifications apportées au répertoire `core`
    
    - `emp_conge` :
    1. Pour ne plus voir le message de warning j'ai modifié le schéma du projet.
    
    ![image.png](https://dev.azure.com/Centre-Expertise-IA/c2f84ed8-4636-4689-a070-433ecb89a643/_apis/git/repositories/c515b32e-0393-45f8-a0f3-3c2ce78033d2/pullRequests/272/attachments/image.png)
    
    2. J'ai également configuré un alias pour que la table générée dans la base de données soit nommée `fact_emp_conge` plutot que `empcong_fact_emp_conge`
    
    ![image (2).png](https://dev.azure.com/Centre-Expertise-IA/c2f84ed8-4636-4689-a070-433ecb89a643/_apis/git/repositories/c515b32e-0393-45f8-a0f3-3c2ce78033d2/pullRequests/272/attachments/image%20%282%29.png)
    
    @<Mohamed Sadqi> , @<Gabriel Thiffault>  Assurez-vous que ces modifications n'aient pas d'impact sur le tableau de bord  (le chemin pour accéder à la table plus précisément).
    
    - `prospectif_cdp` : petite coquille dans le schéma du projet qui générait un warning.
    
    ![image (3).png](https://dev.azure.com/Centre-Expertise-IA/c2f84ed8-4636-4689-a070-433ecb89a643/_apis/git/repositories/c515b32e-0393-45f8-a0f3-3c2ce78033d2/pullRequests/272/attachments/image%20%283%29.png)
    
    - `shared/interfaces/geobus` et `shared/interfaces/piastre` : j'ai fait des modifications afin de respecter les conventions du projet. @<Maryna Zhuravlova>  le nom des champs doit être en minuscule.
    
         ex:
    ![image (4).png](https://dev.azure.com/Centre-Expertise-IA/c2f84ed8-4636-4689-a070-433ecb89a643/_apis/git/repositories/c515b32e-0393-45f8-a0f3-3c2ce78033d2/pullRequests/272/attachments/image%20%284%29.png)
    
    - `transport` :
    
    1. J'ai modifié le nom du script `stg_parc_activ_10y` par `trnsprt_parc_activ_10y`. Tout d'abord, ce n'est pas une table de staging donc ca ne nécessite pas de préfixe `stg`. Finalement, afin d'avoir un schéma centralisé, j'ai rajouté le prefixe `trnsprt_`
    
    2. J'ai également configuré un alias pour que la table généré dans la base de données soit nommé `parc_activ_10y` plutôt que `trnsprt_parc_activ_10y`
    
    ![image (5).png](https://dev.azure.com/Centre-Expertise-IA/c2f84ed8-4636-4689-a070-433ecb89a643/_apis/git/repositories/c515b32e-0393-45f8-a0f3-3c2ce78033d2/pullRequests/272/attachments/image%20%285%29.png)
    
    3. En conséquence, j'ai ajusté le schéma qui d'ailleurs ne fonctionnait pas initialement.
    
    @<Maryna Zhuravlova> , assure-toi que ces modifications n'aient pas d'impact sur le tableau de bord. Je pense que le tableau de bord ne retrouvera pas sa table source sachant que son nom a été modifié
    
    - `dbt_project` : j'ai corrigé la section `transport` du fichier pour harmoniser le processus.
    
    ![image (6).png](https://dev.azure.com/Centre-Expertise-IA/c2f84ed8-4636-4689-a070-433ecb89a643/_apis/git/repositories/c515b32e-0393-45f8-a0f3-3c2ce78033d2/pullRequests/272/attachments/image%20%286%29.png)
    
    - `README` : ajustement de la section `transport`.
    
    @<Maryna Zhuravlova>  je ne comprends pas l'intérêt de la base de données `piastre`. Est-elle vra...

--- models/shared/interfaces/geobus/i_geobus_parc.sql
+++ models/shared/interfaces/geobus/i_geobus_parc.sql
@@ -1,19 +1,20 @@
 
-SELECT Annee --parcour rapport
-      ,Per --parcour rapport
-      ,NoParc AS No_Parc --parcour rapport
-      ,Nom --parcour rapport
-      ,IndActive AS Ind_Active-- active/not active for today
-      ,NbRang  AS Nb_Rang Nb-- for Circuits rapport
-      ,DistTot AS Dist_Tot--financ
-      ,Duree  --financ
-      ,NbPlace AS Nb_Place--circuits rapp
-      ,IndAvisTransp AS Ind_Avis_Transp
-      ,NoCirc AS No_Circ--parcour rapport
-      ,NoSeqTrt AS No_Seq_Trt-- parcour rapport
-      ,HreDeb AS Hre_Deb -- financ rapport
-      ,DistImpro AS Dist_Impro --
-      ,DureeImpro AS Duree_Impro  --duration sec. for finansial calculation
-      ,DateModifParAttrib AS Date_Modif_Par_Attrib-- this date include in the parcoure rapport in a geobus app
+SELECT 
+  Annee AS annee                                    -- parcours rapport
+  , Per AS per                                      -- parcours rapport
+  , NoParc AS no_parc                               -- parcours rapport
+  , Nom AS nom                                      -- parcours rapport
+  , IndActive AS ind_active                         -- active/not active for today
+  , NbRang  AS nb_rang                              -- for Circuits rapport
+  , DistTot AS dist_tot                             -- financ
+  , Duree AS duree                                  -- financ
+  , NbPlace AS nb_place                             -- circuits rapp
+  , IndAvisTransp AS ind_avis_transp
+  , NoCirc AS no_circ                               -- parcours rapport
+  , NoSeqTrt AS no_seq_trt                          -- parcours rapport
+  , HreDeb AS hre_deb                               -- financ rapport
+  , DistImpro AS dist_impro
+  , DureeImpro AS duree_impro                       -- duration sec. for financial calculation
+  , DateModifParAttrib AS date_modif_par_attrib     -- this date include in the parcours rapport in a geobus app
   FROM 
-    {{ var("database_geobus") }}.dbo.GEO_P_PARC
+    {{ var("database_geobus") }}.dbo.geo_p_parc
