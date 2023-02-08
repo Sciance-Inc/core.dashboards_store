56b374ee5e0a3e73ef5cf0043b59672e73a09ea3
sadqim <sadqim@csvdc.qc.ca>
2023-02-08 08:29:01 -0500

    feat : creat_fact_emp_quitter

--- models/shared/interfaces/paie/i_pai_cum_budg.sql
+++ models/shared/interfaces/paie/i_pai_cum_budg.sql
@@ -2,6 +2,6 @@ SELECT
     MATR AS matricule
     , AN_BUDG AS annee_budgetaire
     , CUM as cumulatif 
-    , CORP_EMPL AS corp_emploi    
+    , CORP_EMPL AS corp_empl    
     , CODE_PMNT_DED AS code_pmnt_ded
 FROM  {{ var("database_paie") }}.dbo.PAI_CUM_BUDG
