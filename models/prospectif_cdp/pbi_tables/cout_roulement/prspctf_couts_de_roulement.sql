e17a5029aa34aee8f5f5152ec0d3e1e5800aa42e
sadqim <sadqim@csvdc.qc.ca>
2023-02-23 17:22:29 -0500

    feat : move_table_to_sub_folder_cout_roulement_and_delete_join_on_fact_masse_sal

--- models/prospectif_cdp/pbi_tables/cout_roulement/prspctf_couts_de_roulement.sql
+++ models/prospectif_cdp/pbi_tables/cout_roulement/prspctf_couts_de_roulement.sql
@@ -3,11 +3,8 @@
 WITH src AS (
     SELECT
         msce.*
-        , ms.masse_salariale_an
         , emoq.nb_empl_aremp
     FROM {{ ref('prspctf_fact_masse_sal_corp_empl') }} AS msce
-    LEFT JOIN {{ ref('prspctf_fact_masse_sal') }} AS ms
-        ON msce.annee_budgetaire = ms.annee_budgetaire
     LEFT JOIN {{ ref('prspctf_fact_emp_quitter') }} AS emoq
         ON msce.annee_budgetaire = emoq.annee_budgetaire    
         AND msce.corp_empl = emoq.corp_empl
@@ -15,12 +12,12 @@ WITH src AS (
     SELECT 
         annee_budgetaire
         ,masse_salariale_an
-        ,sum(masse_salariale*nb_empl_aremp) as couts_de_roulement
+        ,sum(sal_moy_corp_emp*nb_empl_aremp) as couts_de_roulement
 
     FROM src
     GROUP BY annee_budgetaire, masse_salariale_an
 )
 SELECT
     annee_budgetaire        
-    ,(couts_de_roulement/masse_salariale_an)*100 AS ratio_couts_roulement
+    ,CAST(((couts_de_roulement/masse_salariale_an)*100) AS DECIMAL(5,2)) AS ratio_couts_roulement
  FROM agg
