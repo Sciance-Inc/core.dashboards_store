b2d02a3c1694a9e13216fbf2820afdb4db2b1db9
sadqim <sadqim@csvdc.qc.ca>
2023-02-23 18:08:10 -0500

    feat : add result table by matiere

---
+++ models/prospectif_cdp/features/res_matieres/prspctf_fact_rslt_mat_select.sql
@@ -0,0 +1,45 @@
+
+
+{{ config(alias='fact_rslt_mat_select') }}
+
+WITH src AS( 
+    SELECT 
+        res.annee
+        , res.fiche
+        , res.ecole
+        , dim.*
+        , res.resultat_numerique 
+    FROM {{ ref('prspctf_dim_mat_selct') }} AS dim
+    LEFT JOIN {{ ref('i_resultats_matieres_eleve')}} AS res
+    ON 
+        dim.code_matiere = res.code_matiere 
+    WHERE resultat_numerique IS NOT NULL 
+)
+
+SELECT 
+    annee
+    , fiche
+    , niveau_scolaire
+    , MAX(res_fr) AS res_fr
+    , MAX(res_maths) AS res_maths    
+    , MAX(res_ang) AS res_ang
+    , MAX(res_sc) AS res_sc
+    , MAX(res_his) AS res_his
+FROM 
+    (
+    SELECT 
+        annee
+        , fiche
+        , niveau_scolaire
+        , CASE WHEN freindly_name = 'Français'  THEN resultat_numerique END AS res_fr
+        , CASE WHEN freindly_name = 'Maths' THEN resultat_numerique END AS res_maths
+        , CASE WHEN freindly_name = 'Anglais'  THEN resultat_numerique END AS res_ang
+        , CASE WHEN freindly_name = 'Science' THEN resultat_numerique END AS res_sc
+        , CASE WHEN freindly_name = 'Histoire' THEN resultat_numerique END AS res_his
+    FROM src
+    
+) AS srctable 
+ GROUP BY 
+        annee
+        , fiche
+        , niveau_scolaire
