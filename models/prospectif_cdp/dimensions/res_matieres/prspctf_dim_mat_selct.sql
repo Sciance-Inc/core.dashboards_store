73de2f5106ebb327ffb586ff5a788aa96c85cee4
sadqim <sadqim@csvdc.qc.ca>
2023-02-23 17:57:44 -0500

    feat : creat  mat_select dimensions table unioning a default and a custom CSV (if exist)

---
+++ models/prospectif_cdp/dimensions/res_matieres/prspctf_dim_mat_selct.sql
@@ -0,0 +1,38 @@
+
+
+{{ config(alias='dim_mat_selct') }}
+
+{%- set source_relation = adapter.get_relation(
+      database=target.database,
+      schema=target.schema +'_prospectif_cdp_seeds',
+      identifier='custom_code_matiere') -%}
+{% set table_exists=source_relation is not none %}
+
+{% if table_exists %}
+    {% if execute %}
+    {{ log("The seed '*_res_prospectif_cdp.custom_code_matiere' DOES EXIST and will be added to the 'default_code_matiere'", true) }}
+    {% endif %}
+
+SELECT 
+    code_matiere,
+    freindly_name,
+    niveau_scolaire
+FROM {{ ref('default_code_matiere') }}
+UNION ALL
+select
+    code_matiere,
+    freindly_name,
+    niveau_scolaire
+from {{ source_relation }} 
+
+{% else %}
+    {% if execute %}
+    {{ log("The seed '_res_prospectif_cdp.custom_code_matiere' DOES NOT exists. The 'prspctf_dim_mat_selct' table will be defaulted to 'default_code_matiere'.", true) }}
+    {% endif %}
+
+SELECT 
+    code_matiere,
+    freindly_name,
+    niveau_scolaire
+FROM {{ ref('default_code_matiere') }}
+{% endif %}
