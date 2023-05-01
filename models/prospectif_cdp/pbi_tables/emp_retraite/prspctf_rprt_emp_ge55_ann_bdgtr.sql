5ef7d3a7f509cfd365d753d645f6a3d97500fe79
Gabriel Thiffault <thiffaultg@csvt.qc.ca>
2023-05-01 13:42:56 +0000

    Merged PR 355: Modifier l'âge de départ à la retraite et prendre supérieur ou égale
    # Objectives of the pull request
    > describe the high level purposes of your pull request. What are you trying to achieve ? How are you doing it ?*
    
    L'objectif était de modifier l'âge du départ à la retraite de 65 à 55 et de ne pas uniquement aller chercher les gens qui avaient 55, mais 55 et plus.
    
    Pour ce faire, nous avons modifié et renommer le fichier **prspctf_rprt_emp65_ann_bdgtr.sql** pour **prspctf_rprt_emp_ge55_ann_bdgtr.sql**
    
    * Avant de rouler cette PR, il faut impérativement excécuter celle de Fréderyk
    
    # How to run the pull request ?
    > Provide the code requiered to run the pull request. This is the code that will be used to review your pull request. **The provided code must work as-is. If a DBT error is raised while running the code, the PR will be rejected.**
    
    ```bash
    # Update the code
    git checkout core.data.tbe/feature/emp_retraite
    git pull
    poetry shell && poetry lock && poetry install
    
    # Run dbt
    dbt seed --full-refresh
    dbt run --select +prspctf_rprt_emp_ge55_ann_bdgtr
    ```
    
    # Pull request's checklist
    * Code :
      * [X] My tables/variables naming follows the conventions described in the `readme.md`.
      * [ ] I have added DBT tests to my models (at least a `non null` / `unique` per model).
    * Documentation :
      * [X]  I have updated the documentation accordingly to my changes.
      * [x]  The models I have added are documented in a `schema.yml` file.
    ![image (2).png](https://dev.azure.com/Centre-Expertise-IA/c2f84ed8-4636-4689-a070-433ecb89a643/_apis/git/repositories/c515b32e-0393-45f8-a0f3-3c2ce78033d2/pullRequests/355/attachments/image%20%282%29.png)
    * Pull request :
      * [x]  The provided code is working.
    ![image.png](https://dev.azure.com/Centre-Expertise-IA/c2f84ed8-4636-4689-a070-433ecb89a643/_apis/git/repositories/c515b32e-0393-45f8-a0f3-3c2ce78033d2/pullRequests/355/attachments/image.png)
      * [x]  I have added my CSS lead as a reviewer.
      * [x]  My pull request is documented.
    * Taskboard :
      * [x] My work item is linked to the pull request.
      * [x] My workw item has been moved to `review` in the taskboard.
    
    chore: change the name of the profile and the project
    
    Related work items: #1770

---
+++ models/prospectif_cdp/pbi_tables/emp_retraite/prspctf_rprt_emp_ge55_ann_bdgtr.sql
@@ -0,0 +1,9 @@
+{{ config(alias='emp_ge55_ann_bdgtr') }}
+SELECT 
+    annee_budgetaire
+    , COUNT(matr) as number_of_permanent_employee
+    , SUM(CONVERT(numeric(10,0),(CASE WHEN age_ann_bdgtr >= 55  THEN 1 ELSE 0 END))) AS number_of_permanent_employee_ge55_years_old
+    , CONVERT(numeric(10,10),AVG(CASE WHEN age_ann_bdgtr >= 55 AND MONTH(date_nais) > 7 THEN 1.0 ELSE 0.0 END)) AS proportion_of_permanent_employee_ge55_years_old
+FROM {{ ref('prspctf_fact_hemp_post_permanant_age') }} 
+WHERE annee_budgetaire >= YEAR(GETDATE()) - 5
+GROUP BY annee_budgetaire
