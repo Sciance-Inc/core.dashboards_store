eda7eefc06b8817a74673c056db7d8288792f7c9
busquef <busquef@cssvt.gouv.qc.ca>
2023-05-26 12:54:36 +0000

    feat: adding the effectif_css dashboard
    > describe the high level purposes of your pull request. What are you trying to achieve ? How are you doing it ?*
    
    Le but de ce tdb est de faire le dénombrement de la population au sein d'un centre de service scolaire. La population étant séparé en 4 : Prescolaire, primaire régulier, primaire adapté, secondaire régulier et secondaire adapté.
    
    Cette population est divisé selon le niveau scolaire de l'élève également. PRES4ANS, PRES5ANS, 1er année à la 6ème année, puis 1er secondaire au 5ème secondaire.
    
    > Provide the code requiered to run the pull request. This is the code that will be used to review your pull request. **The provided code must work as-is. If a DBT error is raised while running the code, the PR will be rejected. The following code / placeholder is only provided as documentation / helper to get you started and you will need to adjust it.**
    
    ```bash
    cd core.data.tbe
    git checkout feature/effectif_css
    git pull
    cd ../cssvt.data.tbe
    git checkout feature/tdb_effectif
    git pull
    dbt build --select tag:effectif_css
    ```
    
    > Please, read carefully each item before checking it. You PR's review might be delayed otherwise.
    
    * **Code** :
      * [X] The code I m asking a review for is working. **I understand that my PR will be rejected as-is otherwise.**
      * [X] My tables/variables naming follows the conventions described in the `readme.md`.
      * [X] I have added DBT tests to my models (at least a `non null` / `unique` per model).
    * **Documentation** :
      * [X]  I have updated the documentation (README) accordingly to my changes.
      * [X]  The models I have added are documented in a `schema.yml` file.
    * **Pull-request** :
      * [X]  The code *provided to run the pull request* is working. **I understand that my PR will be rejected as-is otherwise.**
      * [X]  I have added my CSS lead as a reviewer.
      * [X]  My pull request is documented.
      * [X]  I have carefully reviewd each change made to a file and made sure the files included on the PR has been actually changed on purposes.
    * **Tasksboard** :
      *  [X] My work item is linked to the pull request.
      * [X] My workw item has been moved to `review` in the taskboard.

--- models/marts/educ_serv/spines/spine.sql
+++ models/marts/educ_serv/spines/spine.sql
@@ -1,19 +1,31 @@
 
 
-WITH src AS (
+WITH dan AS (
     SELECT 
-        stg.fiche
-        , stg.annee
-        , stg.id_eco
-        , stg.population
+        dan.fiche
+        , eco.annee
+        , eco.eco
         , dan.date_deb
+    FROM {{ ref('i_gpm_e_dan') }} AS dan
+    LEFT JOIN {{ ref('i_gpm_t_eco') }} AS eco 
+        ON dan.id_eco = eco.id_eco
+    WHERE dan.statut_don_an = 'A'
+),
+
+fiche AS (
+    SELECT 
+        stg.code_perm
+        , stg.annee
+        , ele.fiche
+        , stg.population
     FROM {{ ref('stg_populations_fgj') }} AS stg
-    LEFT JOIN {{ ref('i_gpm_e_dan') }} AS dan 
-        ON stg.fiche = dan.fiche AND stg.id_eco = dan.id_eco
-        
+    LEFT JOIN {{ ref('i_gpm_e_ele') }} AS ele
+        ON stg.code_perm = ele.code_perm
 )
 
 SELECT 
-    src.*
-    , ROW_NUMBER() OVER (PARTITION BY fiche, population ORDER BY date_deb DESC) AS seqid
-FROM src
+    fiche.*
+    , dan.eco
+    , ROW_NUMBER() OVER (PARTITION BY code_perm, fiche.annee, population ORDER BY date_deb DESC) AS seqid
+FROM fiche
+LEFT JOIN dan ON dan.fiche = fiche.fiche AND dan.annee = fiche.annee
