313456e73549d6764a5ffce7ee99b11db9218810
hugo juhel <juhel.hugo@stratemia.com>
2023-08-03 13:15:12 -0400

    fix: the macro no properly delete the schema and the views

--- macros/introspection/drop_schema.sql
+++ macros/introspection/drop_schema.sql
@@ -10,4 +10,26 @@
 {% endset %}
 
 {% do run_query(sql) %}
+
+{% set sql %}
+    DECLARE @SqlStatement NVARCHAR(MAX);
+    SELECT @SqlStatement = 
+        COALESCE(@SqlStatement, N'') + N'DROP VIEW ' + TABLE_SCHEMA + '.' + QUOTENAME(TABLE_NAME) + N';' + CHAR(13)
+    FROM INFORMATION_SCHEMA.TABLES
+    WHERE TABLE_SCHEMA LIKE '%{{ target.schema }}%' and TABLE_TYPE = 'VIEW';
+    EXEC (@SqlStatement);
+{% endset %}
+
+{% do run_query(sql) %}
+
+{% set sql %}
+    DECLARE @SqlStatement NVARCHAR(MAX);
+    SELECT @SqlStatement = 
+        COALESCE(@SqlStatement, N'') + N'DROP SCHEMA ' + QUOTENAME(name) + N';' + CHAR(13)
+    FROM sys.schemas 
+    WHERE name LIKE '%{{ target.schema }}%';
+    EXEC (@SqlStatement);
+{% endset %}
+
+{% do run_query(sql) %}
 {% endmacro %}
