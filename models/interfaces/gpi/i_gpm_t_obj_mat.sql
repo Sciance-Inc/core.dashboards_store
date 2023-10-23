ade16baaa72a4b843ad35c8e6858c41944dac901
sadqim <sadqim@csvdc.qc.ca>
2023-10-23 16:12:30 -0400

    fix: filter_non_null_obj_number

--- models/interfaces/gpi/i_gpm_t_obj_mat.sql
+++ models/interfaces/gpi/i_gpm_t_obj_mat.sql
@@ -1,19 +1,13 @@
-{#
-Dashboards Store - Helping students, one dashboard at a time.
-Copyright (C) 2023  Sciance Inc.
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU Affero General Public License as
-published by the Free Software Foundation, either version 3 of the
-License, or any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU Affero General Public License for more details.
-
-You should have received a copy of the GNU Affero General Public License
-along with this program.  If not, see <https://www.gnu.org/licenses/>.
-#}
-select id_obj_mat, id_eco, mat, obj_01, descr, descr_abreg, descr_det, pond_obj
-from {{ var("database_gpi") }}.dbo.gpm_t_obj_mat
+SELECT 
+  id_obj_mat
+    , id_eco
+    , mat
+    , obj_01
+    , obj_02
+    , obj_03
+    , obj_04
+    , descr
+    , descr_abreg
+    , descr_det
+    , pond_obj
+FROM {{ var("database_gpi") }}.dbo.gpm_t_obj_mat
