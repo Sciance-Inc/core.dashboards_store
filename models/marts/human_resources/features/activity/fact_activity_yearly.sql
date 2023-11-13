cb3aac5e7da3212173cd6660dc7a6777773ba420
hugo juhel <juhel.hugo@stratemia.com>
2023-11-13 14:53:08 -0500

    feat: restoring the INNER JOIN instead of the left join and adding a test to make the seed is populated with all the values to be joined by

--- models/marts/human_resources/features/activity/fact_activity_yearly.sql
+++ models/marts/human_resources/features/activity/fact_activity_yearly.sql
@@ -32,7 +32,7 @@ with
             hst.date_eff,
             hst.date_fin
         from {{ ref("stg_activity_history") }} as hst
-        left join
+        join
             {{ ref("dim_employment_status_yearly") }} as dm
             on hst.school_year = dm.school_year
             and hst.etat_empl = dm.etat_empl
