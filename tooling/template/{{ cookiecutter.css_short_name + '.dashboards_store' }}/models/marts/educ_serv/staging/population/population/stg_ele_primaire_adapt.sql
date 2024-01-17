4d2b35c56c2e1829cdad89c5ddc68df87f5be6cb
hugo juhel <juhel.hugo@stratemia.com>
2024-01-17 10:51:53 -0500

    chore: adding missing licence and formatting the code

--- tooling/template/{{ cookiecutter.css_short_name + '.dashboards_store' }}/models/marts/educ_serv/staging/population/population/stg_ele_primaire_adapt.sql
+++ tooling/template/{{ cookiecutter.css_short_name + '.dashboards_store' }}/models/marts/educ_serv/staging/population/population/stg_ele_primaire_adapt.sql
@@ -1,7 +1,23 @@
+{#
+Dashboards Store - Helping students, one dashboard at a time.
+Copyright (C) 2023  Sciance Inc.
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as
+published by the Free Software Foundation, either version 3 of the
+License, or any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#}
 {# 
     UPDATE THIS FILE TO MATCH YOUR CSS REALITY.
 #}
-
 select distinct ele.code_perm, eco.id_eco, eco.annee
 from {{ ref("i_gpm_e_dan") }} as eledan
 left join {{ ref("i_gpm_t_eco") }} as eco on eledan.id_eco = eco.id_eco
