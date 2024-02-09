91ed524ff12b6e20dc5cbb272ccc7b1d603b5e79
alluardj <alluardj@csvdc.qc.ca>
2024-02-09 13:23:43 -0500

    fix: adjust rule to identify home address

--- models/marts/educ_serv/staging/geospatial/stg_adresses_eleves.sql
+++ models/marts/educ_serv/staging/geospatial/stg_adresses_eleves.sql
@@ -20,40 +20,55 @@ with adr as (
         adr.fiche
         , adr.type_adr
         , cast(adr.date_effect as date) as date_effect
-        , cast(adr.date_fin as date) as date_fin
-        , adr.ind_envoi_meq 
-        , adr.ind_envoi_doc   
-        , adr.ind_envoi_trsp                        
+        , adr.date_fin
+        , adr.ind_envoi_meq                      
         , dim.code_post   
-        , row_number() over (partition by adr.fiche, adr.date_effect order by adr.ind_envoi_meq desc, adr.ind_envoi_trsp desc, adr.ind_envoi_doc desc) as seqid   -- si 2 adresses, on privilegie celle qui est envoyée au meq, transport, doc
+        , row_number() over (partition by fiche order by date_effect) as seqid -- pour identifier la 1ere adresse
     from {{ ref('i_e_adr') }} as adr
     left join {{ ref('dim_adresses') }} as dim
         on dim.no_civ=adr.no_civ and dim.genre_rue=adr.genre_rue and dim.orient_rue=adr.orient_rue and dim.rue=adr.rue and dim.ville=adr.ville
 
--- conserver les dates min / max durant lesquelles le code postale ne change pas
+-- dates avec un ind_envoi_meq de 1 (sauf date initiale)
 ), adr2 as (
-    select distinct
-        fiche
-        , type_adr
-        , code_post
-        , min(date_effect) as date_effect
-        , max(date_fin) as date_fin
+    select *
+    from adr
+    where 
+        ind_envoi_meq = 1
+        and seqid != 1
+
+-- adresses à considerer
+), adr3 as (
+    select *
     from adr
     where seqid=1
-    group by  fiche, type_adr, code_post
+    union all
+    select *
+    from adr2
+
+-- modifier les dates effectives avec les adresses conservées
+), adr4 as (
+    select 
+        fiche
+        , date_effect
+        , case 
+            when (lead(date_effect) over(partition by fiche order by date_effect)) is null then getdate() 
+            else lead(date_effect) over(partition by fiche order by date_effect)
+        end as date_effect_fin
+        , code_post
+    from adr3
 
 -- identifier les annees scolaire d'appartenance de chaque CP
 ), y_sco as (
     select 
         fiche
         , date_effect
-        , date_fin
+        , date_effect_fin
         , case when month(date_effect) <= 6 then year(date_effect) - 1 else year(date_effect)
 		end as annee_sco_deb
-	    , case when  month(date_fin) < 9 then year(date_fin) - 1 else year(date_fin)
+	    , case when  month(date_effect_fin) < 9 then year(date_effect_fin) - 1 else year(date_effect_fin)
 		end as annee_sco_fin
         , code_post
-    from adr2
+    from adr4
 
 -- recuperer les annees scolaire de debut et de fin pour chaque fiche
 ), tab  as (
@@ -77,10 +92,8 @@ with adr as (
 select
     long.fiche
     , long.annee
-    --, y_sco.date_effect
-    --, y_sco.date_fin
     , y_sco.code_post
-    , case when datefromparts(long.annee, 9, 30) between y_sco.date_effect and y_sco.date_fin then 1 else 0 end as adresse_30sept
+    , case when datefromparts(long.annee, 9, 30) between y_sco.date_effect and y_sco.date_effect_fin then 1 else 0 end as adresse_30sept
     , row_number() over (partition by long.fiche, long.annee order by y_sco.date_effect desc) as seqid
 from long
 left join y_sco
