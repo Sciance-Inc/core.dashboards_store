c639b80c39060d49f68ba8885d432081e6870a9f
hugoJuhel <juhel.hugo@sciance.ca>
2024-06-20 11:53:20 -0400

    chore(template): improving default populations

--- tooling/template/{{ cookiecutter.css_short_name + '.dashboards_store' }}/models/marts/educ_serv/staging/population/population/stg_ele_prescolaire.sql
+++ tooling/template/{{ cookiecutter.css_short_name + '.dashboards_store' }}/models/marts/educ_serv/staging/population/population/stg_ele_prescolaire.sql
@@ -22,8 +22,9 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.
 select distinct ele.code_perm, eco.id_eco, eco.annee
 from {{ ref("i_gpm_e_dan") }} as eledan
 left join {{ ref("i_gpm_t_eco") }} as eco on eledan.id_eco = eco.id_eco
-left join
-    {{ ref("i_gpm_e_ele") }} as ele on eledan.fiche = ele.fiche
+left join {{ ref("i_gpm_e_ele") }} as ele on eledan.fiche = ele.fiche
+where
+    eledan.statut_don_an = 'A' and eledan.ordre_ens in ('1', '2')
     /*WHERE 
     eledan.statut_don_an = 'A' AND (
         (
