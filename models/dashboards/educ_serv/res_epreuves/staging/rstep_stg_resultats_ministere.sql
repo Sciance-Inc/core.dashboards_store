5036ffe001fe5f3b97dbdd9628fe6178465003ac
hugoJuhel <juhel.hugo@sciance.ca>
2024-10-17 14:35:49 -0400

    fix(res_epreuves): fixing a typo in the outputed table name

--- models/dashboards/educ_serv/res_epreuves/staging/rstep_stg_resultats_ministere.sql
+++ models/dashboards/educ_serv/res_epreuves/staging/rstep_stg_resultats_ministere.sql
@@ -20,7 +20,7 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.
 #}
 {{
     config(
-        alias="stg_resultat_minstere",
+        alias="stg_resultat_ministere",
     )
 }}
 
