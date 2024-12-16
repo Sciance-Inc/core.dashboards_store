c800d9c0572215356aa1f66064202b6776774511
AlimuratDinch <alimrtdinch312@gmail.com>
2024-12-16 11:58:39 -0500

    feat: template de power bi pour tdb anomalies

--- models/dashboards/org_scolaire/anomalies/pbi_tables/anml_report_eleves_doublons_actifs.sql
+++ models/dashboards/org_scolaire/anomalies/pbi_tables/anml_report_eleves_doublons_actifs.sql
@@ -1,4 +1,21 @@
 {#
+Dashboards Store - Helping students, one dashboard at a time.
+Copyright (C) 2023  Sciance Inc.
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as
+published by the Free Software Foundation, either version 3 of the
+License, or any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#}
+{#
     Trouver des élèves actives qui étaient actives deux fois par l''année
 #}
 {{ config(alias="report_eleves_doublons_actifs") }}
