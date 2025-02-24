b9f0f98e78905b10d35d5bddd1ca05d7d6d25938
Frederyk Busque <147734849+busquef@users.noreply.github.com>
2025-02-24 15:21:56 -0500

    feat: Ajoût de l'indicateur épreuve et de l'indicateur PPP (#3)
    * feat : ajout des seeds
    
    * feat :  ajout des table de dim et pbi plus modification du dbt_project
    
    * feat ajout du pbit
    
    * feat: ajout cible
    
    ---------
    
    Co-authored-by: sadqim <sadqim@csvdc.qc.ca>

---
+++ models/dashboards/other/pevr/pbi_tables/pevr_indicateur_epreuves.sql
@@ -0,0 +1,166 @@
+{#
+Dashboards Store - Helping students, one dashboard at a time.
+Copyright (C) 2023  Sciance Inc.
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as
+published by the Free Software Foundation, either version 3 of the
+License, or any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#}
+{{ config(alias="indicateur_epreuves") }}
+
+
+with
+    src as (
+        select
+            case
+                when ind.id_indicateur_css is null
+                then ind.id_indicateur_cdpvd  -- Permet d'utiliser l'indicateur défaut de la CDPVD
+                else ind.id_indicateur_css
+            end as id_indicateur,
+            ind.description_indicateur,
+            ind.cible,
+            res.annee,
+            sch.annee_scolaire,
+            res.fiche,
+            case
+                when sch.school_friendly_name is null
+                then '-'
+                else sch.school_friendly_name
+            end as school_friendly_name,
+            case when el.genre is null then '-' else el.genre end as genre,
+            case
+                when y_stud.plan_interv_ehdaa is null
+                then '-'
+                else y_stud.plan_interv_ehdaa
+            end as plan_interv_ehdaa,
+            case
+                when y_stud.population is null then '-' else y_stud.population
+            end as population,
+            case
+                when y_stud.class is null then '-' else y_stud.class
+            end as classification,
+            case when y_stud.dist is null then '-' else y_stud.dist end as distribution,
+            ind.code_matiere,
+            ind.no_competence,
+            etape,
+            cast(is_maitrise as decimal(2, 1)) is_maitrise
+        from {{ ref("fact_resultat_etape_competence") }} as res
+        left join
+            {{ ref("fact_yearly_student") }} as y_stud
+            on y_stud.fiche = res.fiche
+            and y_stud.id_eco = res.id_eco
+        inner join {{ ref("dim_mapper_schools") }} as sch on res.id_eco = sch.id_eco
+        inner join {{ ref("dim_eleve") }} as el on res.fiche = el.fiche
+        inner join
+            {{ ref("pevr_dim_indicateurs") }} as ind
+            on res.code_matiere = ind.code_matiere
+            and res.no_comp = ind.no_competence
+        where
+            res.annee
+            between {{ core_dashboards_store.get_current_year() }}
+            - 3 and {{ core_dashboards_store.get_current_year() }}
+            and etape = 'EX'
+            and id_indicateur_cdpvd in ('4','5','6')  -- Au cas-où qu'on utilise le champs code_matière pour d'autre indicateur
+    ),
+
+    agg as (
+        select
+            id_indicateur,
+            description_indicateur,
+            cible,
+            annee_scolaire,
+            school_friendly_name,
+            genre,
+            plan_interv_ehdaa,
+            population,
+            classification,
+            distribution,
+            code_matiere,
+            count(fiche) nb_resultat,
+            cast(avg(is_maitrise) as decimal(5, 3)) as taux_maitrise,
+            cast(((avg(is_maitrise)) - cible) as decimal(5, 3)) as ecart_cible
+        from src
+        group by
+            id_indicateur,
+            description_indicateur,
+            annee_scolaire,
+            code_matiere,
+            cible, cube (
+                school_friendly_name,
+                genre,
+                plan_interv_ehdaa,
+                population,
+                classification,
+                distribution
+            )
+    ),
+
+    _coalesce as (
+        select
+            id_indicateur,
+            description_indicateur,
+            annee_scolaire,
+            coalesce(school_friendly_name, 'CSS') as ecole,
+            coalesce(genre, 'Tout') as genre,
+            coalesce(plan_interv_ehdaa, 'Tout') as plan_interv_ehdaa,
+            coalesce(population, 'Tout') as population,
+            coalesce(classification, 'Tout') as classification,
+            coalesce(distribution, 'Tout') as distribution,
+            nb_resultat,
+            taux_maitrise,
+            ecart_cible,
+            cible
+        from agg
+    ),id_filtre as (
+        select
+            objectif,
+            cib.id_indicateur,
+            ind.description_indicateur,
+            cib.annee_scolaire,
+            id.nb_resultat,
+            id.taux_maitrise,  -- Possibilité d'avoir un null à cause du res_etape_num peut être nulle. A voir.
+            id.ecart_cible,  -- Même affaire.
+            cib.cible,
+            {{
+                dbt_utils.generate_surrogate_key(
+                    [
+                        "ecole",
+                        "cib.annee_scolaire",
+                        "plan_interv_ehdaa",
+                        "genre",
+                        "population",
+                        "classification",
+                        "distribution",
+                    ]
+                )
+            }} as id_filtre
+        from  {{ ref("pevr_dim_cibles_annuelles") }} cib 
+        left join  {{ ref("pevr_dim_indicateurs") }}  as ind
+            on ind.id_indicateur_css = cib.id_indicateur
+        left join _coalesce as id
+            on id.id_indicateur = cib.id_indicateur and  id.annee_scolaire = cib.annee_scolaire
+
+    )
+
+select 
+	objectif,
+    id_indicateur,
+    description_indicateur,
+    case when annee_scolaire = '2022 - 2023' then 'Valeur de départ' else annee_scolaire end as annee_scolaire,
+    nb_resultat,
+    taux_maitrise,  -- Possibilité d'avoir un null à cause du res_etape_num peut être nulle. A voir.
+    ecart_cible,  -- Même affaire.
+    cible,
+    id_filtre
+from  id_filtre as id
+
+
---
+++ models/dashboards/other/pevr/pbi_tables/pevr_indicateur_ppp.sql
@@ -0,0 +1,167 @@
+{#
+Dashboards Store - Helping students, one dashboard at a time.
+Copyright (C) 2023  Sciance Inc.
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as
+published by the Free Software Foundation, either version 3 of the
+License, or any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#}
+{{ config(alias="indicateur_ppp") }}
+
+with
+    src as (
+        select
+            '11' as id_indicateur,
+            sch.annee_scolaire,
+            sch.annee,
+            case
+                when sch.school_friendly_name is null
+                then '-'
+                else sch.school_friendly_name
+            end as school_friendly_name,
+            case when ele.genre is null then '-' else ele.genre end as genre,
+            case
+                when y_stud.plan_interv_ehdaa is null
+                then '-'
+                else y_stud.plan_interv_ehdaa
+            end as plan_interv_ehdaa,
+            case
+                when y_stud.population is null then '-' else y_stud.population
+            end as population,
+            case
+                when y_stud.class is null then '-' else y_stud.class
+            end as classification,
+            case when y_stud.dist is null then '-' else y_stud.dist end as distribution,
+            case when y_stud.is_ppp = 1 then 1. else 0. end as is_ppp
+        from {{ ref("fact_yearly_student") }} y_stud
+        inner join {{ ref("dim_eleve") }} as ele on y_stud.fiche = ele.fiche
+        inner join {{ ref("dim_mapper_schools") }} as sch on y_stud.id_eco = sch.id_eco
+        where
+            ordre_ens = 4
+            and sch.annee
+            between {{ core_dashboards_store.get_current_year() }}
+            - 3 and {{ core_dashboards_store.get_current_year() }}
+    ),
+
+    ind_pevr as (
+        select
+            case
+                when ind.id_indicateur_css is null
+                then ind.id_indicateur_cdpvd  -- Permet d'utiliser l'indicateur défaut de la CDPVD
+                else ind.id_indicateur_css
+            end as id_indicateur,
+            ind.description_indicateur,
+            ind.cible,
+            is_ppp,
+            annee_scolaire,
+            school_friendly_name,
+            genre,
+            plan_interv_ehdaa,
+            population,
+            classification,
+            distribution
+        from src
+        inner join
+            {{ ref("pevr_dim_indicateurs") }} as ind
+            on src.id_indicateur = ind.id_indicateur_cdpvd
+    ),
+
+    ppp as (
+        select
+            annee_scolaire,
+            school_friendly_name,
+            genre,
+            plan_interv_ehdaa,
+            population,
+            classification,
+            distribution,
+            id_indicateur,
+            description_indicateur,
+            sum(is_ppp) as nb_ppp,
+            avg(is_ppp) as taux_ppp,
+            cast(((avg(is_ppp)) - cible) as decimal(5, 3)) as ecart_cible,
+            cible
+        from ind_pevr
+        group by
+            annee_scolaire,
+            id_indicateur,
+            description_indicateur,
+            cible, cube (
+                school_friendly_name,
+                genre,
+                plan_interv_ehdaa,
+                population,
+                classification,
+                distribution
+            )
+    ),
+
+    _coalesce as (
+        select
+            id_indicateur,
+            description_indicateur,
+            annee_scolaire,
+            coalesce(school_friendly_name, 'CSS') as ecole,
+            coalesce(genre, 'Tout') as genre,
+            coalesce(plan_interv_ehdaa, 'Tout') as plan_interv_ehdaa,
+            coalesce(population, 'Tout') as population,
+            coalesce(classification, 'Tout') as classification,
+            coalesce(distribution, 'Tout') as distribution,
+            nb_ppp,
+            taux_ppp,
+            ecart_cible,
+            cible
+        from ppp
+    ),
+
+    id_filtre as (
+        select
+            objectif,
+            cib.id_indicateur,
+            ind.description_indicateur,
+            cib.annee_scolaire,
+            nb_ppp,
+            taux_ppp,
+            ecart_cible,
+            cib.cible,
+            {{
+                dbt_utils.generate_surrogate_key(
+                    [
+                        "ecole",
+                        "cib.annee_scolaire",
+                        "plan_interv_ehdaa",
+                        "genre",
+                        "population",
+                        "classification",
+                        "distribution",
+                    ]
+                )
+            }} as id_filtre
+        from  {{ ref("pevr_dim_cibles_annuelles") }} cib 
+        left join  {{ ref("pevr_dim_indicateurs") }}  as ind
+            on ind.id_indicateur_css = cib.id_indicateur
+        left join _coalesce as id
+            on id.id_indicateur = cib.id_indicateur and  id.annee_scolaire = cib.annee_scolaire
+        
+    )
+select
+	objectif,
+    id.id_indicateur,
+    description_indicateur,
+    case when annee_scolaire = '2022 - 2023' then 'Valeur de départ' else annee_scolaire end as annee_scolaire,
+    taux_ppp,
+    nb_ppp,
+    ecart_cible,
+    cible,
+    id_filtre
+from id_filtre as id
+
