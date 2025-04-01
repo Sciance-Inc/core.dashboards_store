ca1a656aea182b44865b7ba95af052c45107b747
Adama Fall <fallada@cs-soreltracy.qc.ca>
2025-04-01 15:26:24 -0400

    Fix: mise à jour de l'année de copyright dans stg_check_passepartout.sql

--- models/marts/educ_serv/staging/populations/stg_check_passepartout.sql
+++ models/marts/educ_serv/staging/populations/stg_check_passepartout.sql
@@ -1,6 +1,6 @@
 {#
 Dashboards Store - Helping students, one dashboard at a time.
-Copyright (C) 2025  Sciance Inc.
+Copyright (C) 2023  Sciance Inc.
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU Affero General Public License as
