9f3a5ce9cfe36f0cb54801032c5272eba7fba0c8
girouardg <162153599+girouardg@users.noreply.github.com>
2025-04-14 17:53:25 -0400

    feat(diplome): adding new course CCQ to diploma tracking (#81)
    Co-authored-by: sadqim <146247957+sadqim@users.noreply.github.com>
    Co-authored-by: sadqim <sadqim@csvdc.qc.ca>
    Co-authored-by: Hugo Juhel <22279443+hugoJuhel@users.noreply.github.com>

--- models/dashboards/educ_serv/diplome/pbi_tables/dip_report_suivi_diplome.sql
+++ models/dashboards/educ_serv/diplome/pbi_tables/dip_report_suivi_diplome.sql
@@ -325,36 +325,51 @@ with
             string_agg(
                 case
                     when
-                        regroupement_matière = 'Complémentaire 5 (Eth)'
+                        regroupement_matière = 'Complémentaire 5 (ECR)'
                         and en_cours = '0'
                     then convert(nvarchar, resultat)
                     when
-                        regroupement_matière = 'Complémentaire 5 (Eth)'
+                        regroupement_matière = 'Complémentaire 5 (ECR)'
                         and en_cours = '1'
                     then concat(convert(nvarchar, resultat), ' (En cours)')
                 end,
                 ', '
-            ) as res_compl_5_eth,  -- Le résultat en Complémentaire 5 (Eth)
+            ) as res_compl_5_eth,  -- Le résultat en Complémentaire 5 (ECR)
             string_agg(
                 case
                     when
-                        regroupement_matière = 'Complémentaire 5 (Éduc)'
+                        regroupement_matière = 'Complémentaire 5 (CCQ)'
                         and en_cours = '0'
                     then convert(nvarchar, resultat)
                     when
-                        regroupement_matière = 'Complémentaire 5 (Éduc)'
+                        regroupement_matière = 'Complémentaire 5 (CCQ)'
                         and en_cours = '1'
                     then concat(convert(nvarchar, resultat), ' (En cours)')
                 end,
                 ', '
-            ) as res_compl_5_éduc,  -- Le résultat en Complémentaire 5 (Éduc)
+            ) as res_compl_5_ccq,  -- Le résultat en Complémentaire 5 (CCQ)
+            string_agg(
+                case
+                    when
+                        regroupement_matière = 'Complémentaire 5 (EPS)'
+                        and en_cours = '0'
+                    then convert(nvarchar, resultat)
+                    when
+                        regroupement_matière = 'Complémentaire 5 (EPS)'
+                        and en_cours = '1'
+                    then concat(convert(nvarchar, resultat), ' (En cours)')
+                end,
+                ', '
+            ) as res_compl_5_éduc,  -- Le résultat en Complémentaire 5 (EPS)
             max(
                 case
                     when
                         (
-                            regroupement_matière = 'Complémentaire 5 (Eth)'
+                            regroupement_matière = 'Complémentaire 5 (ECR)'
                             and (ind_reussite = 'RE' or ind_reussite = 'R')
-                            or regroupement_matière = 'Complémentaire 5 (Éduc)'
+                            or regroupement_matière = 'Complémentaire 5 (CCQ)'
+                            and (ind_reussite = 'RE' or ind_reussite = 'R')
+                            or regroupement_matière = 'Complémentaire 5 (EPS)'
                             and (ind_reussite = 'RE' or ind_reussite = 'R')
                         )
                     then 1
@@ -512,6 +527,7 @@ select
     res_compl_4_art_d,
     res_compl_4_danse,
     res_compl_5_eth,
+    res_compl_5_ccq,
     res_compl_5_éduc,
     nb_unites_acquis_g4,
     nb_unites_g4_en_cours,
