4a6dd1586fad8805921cfefa138c6194dc148ae5
Gabriel Thiffault <thiffaultg@csvt.qc.ca>
2025-10-31 11:47:34 -0400

    chore: Ajout de le dimension mapper : job_class

---
+++ models/marts/human_resources/dimensions/mappers/dim_mapper_job_class.sql
@@ -0,0 +1,19 @@
+{#
+CDPVD Dashboards store
+Copyright (C) 2024 CDPVD.
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as
+published by the Free Software Foundation, either version 3 of the
+License, or any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#}
+select corp_empl as code_job, concat(corp_empl, ' - (', descr, ')') as code_job_name
+from {{ ref("i_pai_tab_corp_empl") }}
