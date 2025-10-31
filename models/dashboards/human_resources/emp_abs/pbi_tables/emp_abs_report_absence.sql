3391d21584ad960fadceecd7fb15bdcad8c5a6a0
Gabriel Thiffault <thiffaultg@csvt.qc.ca>
2025-10-31 12:04:26 -0400

    feat: Ajout de la table pbi_table

---
+++ models/dashboards/human_resources/emp_abs/pbi_tables/emp_abs_report_absence.sql
@@ -0,0 +1,113 @@
+{#
+CDPVD Dashboards store
+Copyright (C) 2024 CDPVD.
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as
+published by the Free Software Foundation, either version 3 of the
+License, or any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#}
+{{
+    config(
+        alias="report_absence",
+        materialized="table",
+        post_hook=[
+            core_dashboards_store.create_clustered_index(
+                "{{ this }}", ["annee", "matricule"]
+            ),
+            core_dashboards_store.create_nonclustered_index(
+                "{{ this }}", ["date", "corp_empl", "lieu_trav"]
+            ),
+            core_dashboards_store.create_nonclustered_index(
+                "{{ this }}", ["secteur", "cat_emp"]
+            ),
+            core_dashboards_store.stamp_model("dashboard_emp_abs"),
+        ],
+    )
+}}
+
+with
+    absences_employe_final as (
+        select
+            concat(left(abs.annee, 4), '-', left(abs.annee, 4) + 1) as annee,
+            abs.matricule,
+            emp.legal_name as 'nom',
+            emp.sex_friendly_name as genre,
+            case
+                when
+                    datediff(
+                        year, emp.birth_date, cast(left(annee, 4) + '-07-01' as date)
+                    )
+                    < 25
+                then '24 ans et moins'
+                when
+                    datediff(
+                        year, emp.birth_date, cast(left(annee, 4) + '-07-01' as date)
+                    )
+                    between 25 and 34
+                then '25 à 34 ans'
+                when
+                    datediff(
+                        year, emp.birth_date, cast(left(annee, 4) + '-07-01' as date)
+                    )
+                    between 35 and 44
+                then '35 à 44 ans'
+                when
+                    datediff(
+                        year, emp.birth_date, cast(left(annee, 4) + '-07-01' as date)
+                    )
+                    between 45 and 54
+                then '45 à 54 ans'
+                when
+                    datediff(
+                        year, emp.birth_date, cast(left(annee, 4) + '-07-01' as date)
+                    )
+                    between 55 and 64
+                then '55 à 64 ans'
+                else '65 ans et plus'
+            end as tranche_age,
+            datediff(
+                year, emp.birth_date, cast(left(annee, 4) + '-07-01' as date)
+            ) as age,
+            jc.code_job_name as corp_empl,
+            wp.workplace_name as lieu_trav,
+            sec.secteur_descr as secteur,
+            jg.job_group_category as cat_emp,
+            abs.categories,
+            jds_lundi,
+            jds_mardi,
+            jds_mercredi,
+            jds_jeudi,
+            jds_vendredi,
+            duree_descr,
+            date,
+            jour_absence,
+            jour_travaille,
+            hr_abs,
+            etc_abs
+        from {{ ref("fact_absence_e2") }} as abs
+
+        inner join {{ ref("dim_employees") }} as emp on abs.matricule = emp.matr
+
+        inner join {{ ref("secteur") }} as sec on abs.lieu_trav = sec.ua_id
+
+        inner join
+            {{ ref("dim_mapper_job_group") }} as jg on abs.corp_empl = jg.job_group
+
+        inner join
+            {{ ref("dim_mapper_job_class") }} as jc on abs.corp_empl = jc.code_job
+
+        inner join
+            {{ ref("dim_mapper_workplace") }} as wp on abs.lieu_trav = wp.workplace
+    )
+
+select *
+from absences_employe_final
