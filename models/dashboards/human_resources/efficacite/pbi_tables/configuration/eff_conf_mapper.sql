7ed823e259e273354a31d77103cb1ce6be58f562
busquef <busquef@csvt.qc.ca>
2026-01-22 09:19:09 -0500

    feat(efficacite): ajout des tables de configuration

---
+++ models/dashboards/human_resources/efficacite/pbi_tables/configuration/eff_conf_mapper.sql
@@ -0,0 +1,26 @@
+{#
+Dashboards Store - Helping students, one dashboard at a time.
+Copyright (C) 2023  Sciance Inc.
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as
+published by the Free Software Foundation, either version 3 of the
+License, or any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#}
+Select 
+    lieu_jumele,
+    nom_lieu_jumele,
+    categorie_lieu_jumele,
+    case 
+        when is_school_comparable = 1 then 'Oui'
+        else 'Non'
+    end as comparable
+from {{ ref('dim_mapper_lieu_jumele') }}
