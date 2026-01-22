059b0b1bdd3e5fe9ac99bb0d4500c44f614bb6ab
busquef <busquef@csvt.qc.ca>
2026-01-22 09:19:09 -0500

    feat(efficacite): ajout des tables de configuration

--- models/dashboards/human_resources/efficacite/pbi_tables/configuration/eff_conf_comparabilite_eco_reg.sql
+++ models/dashboards/human_resources/efficacite/pbi_tables/configuration/eff_conf_comparabilite_eco_reg.sql
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 {#
 Dashboards Store - Helping students, one dashboard at a time.
 Copyright (C) 2023  Sciance Inc.
@@ -21,3 +22,32 @@ select distinct
     nom_lieu_jumele,
     categorie_lieu_jumele
 from {{ ref("eff_reporting_configuration") }}
+=======
+{#
+Dashboards Store - Helping students, one dashboard at a time.
+Copyright (C) 2023  Sciance Inc.
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as
+published by the Free Software Foundation, either version 3 of the
+License, or any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#}
+
+select distinct
+    lieu_jumele,
+    case 
+        when is_school_comparable = 1 then 'Oui'
+        else 'Non'
+    end as comparable,
+    nom_lieu_jumele,
+    categorie_lieu_jumele
+from {{ ref('eff_reporting_configuration') }}
+>>>>>>> 7ed823e (feat(efficacite): ajout des tables de configuration)
--- models/dashboards/human_resources/efficacite/pbi_tables/configuration/eff_conf_mapper.sql
+++ models/dashboards/human_resources/efficacite/pbi_tables/configuration/eff_conf_mapper.sql
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 {#
 Dashboards Store - Helping students, one dashboard at a time.
 Copyright (C) 2023  Sciance Inc.
@@ -21,3 +22,31 @@ select
     categorie_lieu_jumele,
     case when is_school_comparable = 1 then 'Oui' else 'Non' end as comparable
 from {{ ref("dim_mapper_lieu_jumele") }}
+=======
+{#
+Dashboards Store - Helping students, one dashboard at a time.
+Copyright (C) 2023  Sciance Inc.
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as
+published by the Free Software Foundation, either version 3 of the
+License, or any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#}
+Select 
+    lieu_jumele,
+    nom_lieu_jumele,
+    categorie_lieu_jumele,
+    case 
+        when is_school_comparable = 1 then 'Oui'
+        else 'Non'
+    end as comparable
+from {{ ref('dim_mapper_lieu_jumele') }}
+>>>>>>> 7ed823e (feat(efficacite): ajout des tables de configuration)
