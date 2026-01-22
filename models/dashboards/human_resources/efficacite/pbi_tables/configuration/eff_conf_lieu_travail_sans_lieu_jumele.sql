533e452c6421658156a2acab0f1132b330558034
busquef <busquef@csvt.qc.ca>
2026-01-22 09:19:09 -0500

    feat(efficacite): ajout des tables de configuration

---
+++ models/dashboards/human_resources/efficacite/pbi_tables/configuration/eff_conf_lieu_travail_sans_lieu_jumele.sql
@@ -0,0 +1,24 @@
+{#
+Dashboards Store - Helping students, one dashboard at a time.
+Copyright (C) 2023  Sciance Inc.
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as
+published by the Free Software Foundation, either version 3 of the
+License, or any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#}
+select distinct
+    lt.lieu_trav,
+    lt.descr
+from {{ ref('i_pai_tab_lieu_trav') }} lt
+left join {{ ref('eff_mapping_fgj_paie') }} mp
+    on lt.lieu_trav = mp.lieu_trav
+where mp.lieu_trav is null
