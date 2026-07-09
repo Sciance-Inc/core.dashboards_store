949ce9b9d05860a0ada038afe5f16a2ca4db1333
hugoJuhel <juhel.hugo@sciance.ca>
2026-07-09 12:51:34 -0400

    fix(paiement): the check date is now pulled from the right table

--- models/interfaces/paie/i_pai_hchq.sql
+++ models/interfaces/paie/i_pai_hchq.sql
@@ -15,4 +15,6 @@ GNU Affero General Public License for more details.
 You should have received a copy of the GNU Affero General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>.
 #}
-select matr, no_cheq, an_budg from {{ var("database_paie") }}.dbo.pai_hchq with (nolock)
+select matr, no_cheq, cast(date_cheq as date) as date_cheq, an_budg
+from {{ var("database_paie") }}.dbo.pai_hchq
+with (nolock)
