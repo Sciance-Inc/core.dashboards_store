dd3cc1562667819802f69bd7010da2f750f02cd5
hugoJuhel <juhel.hugo@sciance.ca>
2026-08-25 12:01:58 -0400

    style(efficacite): fix analysis checks

--- analyses/dashboards/efficacite/bootstrap_eff_mapping_fgj_paie.sql
+++ analyses/dashboards/efficacite/bootstrap_eff_mapping_fgj_paie.sql
@@ -1,3 +1,20 @@
+{#
+Dashboards Store - Helping students, one dashboard at a time.
+Copyright (C) 2023  Sciance Inc.
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as
+published by the Free Software Foundation, either version 3 of the
+License, or any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#}
 -- Génère les lignes valides de la seed eff_mapping_fgj_paie.
 -- Convention : le lieu jumelé est le code école GPI lorsque la liaison est exacte.
 with
@@ -15,9 +32,6 @@ with
         group by lieu_trav
     )
 
-select
-    paie.lieu_trav,
-    gpi.ecole_gpi,
-    gpi.ecole_gpi as lieu_jumele
+select paie.lieu_trav, gpi.ecole_gpi, gpi.ecole_gpi as lieu_jumele
 from gpi
 inner join paie on paie.lieu_trav = gpi.ecole_gpi
